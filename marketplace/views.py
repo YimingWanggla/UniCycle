467c22ddc2a8f46ea822fa139cc9877a5a2b58eb
“Yiming <3088495W@student.gla.ac.uk>
2026-03-17 08:29:21 +0000

    Implement dynamic homepage to display listings (M3 requirement)

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from .models import Listing
 
 def index(request):
-    return render(request, 'marketplace/index.html')
+    # 从数据库里捞出所有状态为 'Available' 的物品，并且按时间倒序排（最新的在最上面）
+    recent_listings = Listing.objects.filter(status='Available').order_by('-created')
+    
+    # 把捞出来的数据打包成一个字典，准备发给网页
+    context = {
+        'listings': recent_listings
+    }
+    
+    # 把数据传递给 index.html
+    return render(request, 'marketplace/index.html', context)
